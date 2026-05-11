2d8edef73d8a5a8a146eeee137fc78900d3c0580
Nishant Narudkar <nishnarudkar@gmail.com>
2026-05-11 16:55:31 +0530

    Creation of the screens

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -2,3 +2,14 @@ import streamlit as st
 
 def home_screen():
     st.header("Home Screen")
+    col1 , col2 = st.columns(2)
+
+    with col1:
+        if st.button("Teacher portal"):
+            st.session_state['login_type'] = "teacher"
+            st.rerun()
+    
+    with col2:
+        if st.button("Student portal"):
+            st.session_state['login_type'] = "student"
+            st.rerun()
